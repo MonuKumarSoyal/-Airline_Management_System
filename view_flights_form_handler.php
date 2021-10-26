200e343b734dcfc212d08a6007d54eeb6083478a
Mahadev <93091309+MonuKumarSoyal@users.noreply.github.com>
2021-10-26 21:34:54 +0530

    Add files via upload

---
+++ view_flights_form_handler.php
@@ -0,0 +1,250 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<title>
+			View Available Flights
+		</title>
+		<style>
+			input {
+    			border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 7px 30px;
+			}
+			input[type=submit] {
+				background-color: #030337;
+				color: white;
+    			border-radius: 4px;
+    			padding: 7px 45px;
+    			margin: 0px 390px
+			}
+			table {
+			 border-collapse: collapse; 
+			}
+			tr/*:nth-child(3)*/ {
+			 border: solid thin;
+			}
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+	</head>
+	<body>
+		<img class="logo" src="images/shutterstock_22.jpg"/> 
+		<h1 id="title">
+			FLIGHTJET AIRLINES
+		</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <span class="nav-link active" >Flightjet</span>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Login
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="login.php">Login</a></li>
+                  <li><a class="dropdown-item" href="new_user.php">Sign up</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Log out</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Contact us</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+		<h2>AVAILABLE FLIGHTS</h2>
+		<?php
+			if(isset($_POST['Search']))
+			{
+				$data_missing=array();
+				if(empty($_POST['origin']))
+				{
+					$data_missing[]='Origin';
+				}
+				else
+				{
+					$origin=$_POST['origin'];
+				}
+				if(empty($_POST['destination']))
+				{
+					$data_missing[]='Destination';
+				}
+				else
+				{
+					$destination=$_POST['destination'];
+				}
+
+				if(empty($_POST['dep_date']))
+				{
+					$data_missing[]='Departure Date';
+				}
+				else
+				{
+					$dep_date=trim($_POST['dep_date']);
+				}
+				if(empty($_POST['no_of_pass']))
+				{
+					$data_missing[]='No. of Passengers';
+				}
+				else
+				{
+					$no_of_pass=trim($_POST['no_of_pass']);
+				}
+
+				if(empty($_POST['class']))
+				{
+					$data_missing[]='Class';
+				}
+				else
+				{
+					$class=trim($_POST['class']);
+				}
+
+				if(empty($data_missing))
+				{
+					$_SESSION['no_of_pass']=$no_of_pass;
+					$_SESSION['class']=$class;
+					$count=1;
+					$_SESSION['count']=$count;
+					$_SESSION['journey_date']=$dep_date;
+					// require_once('Database Connection file/mysqli_connect.php');
+                    DEFINE('DB_USER','root');
+                    DEFINE('DB_PASSWORD','');
+                    DEFINE('DB_HOST','localhost');
+                    DEFINE('DB_NAME','flightjet');
+
+                    $dbc=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
+                    OR dies('Could not connect to MySQL:' .
+                        mysqli_connect_error());
+
+                    // $query="SELECT * FROM customer where customer_id='".$user_name."' and pwd='".$pass_word."'";
+                    // $result123 = mysqli_query($dbc,$query);
+                    // $final = mysqli_num_rows($result123);
+					if($class=="economy")
+					{
+						$query="SELECT flight_no,from_city,to_city,departure_date,departure_time,arrival_date,arrival_time,price_economy FROM Flight_Details where from_city=? and to_city=? and departure_date=? and seats_economy>=? ORDER BY  departure_time";
+						$stmt=mysqli_prepare($dbc,$query);
+						mysqli_stmt_bind_param($stmt,"sssi",$origin,$destination,$dep_date,$no_of_pass);
+						mysqli_stmt_execute($stmt);
+						mysqli_stmt_bind_result($stmt,$flight_no,$from_city,$to_city,$departure_date,$departure_time,$arrival_date,$arrival_time,$price_economy);
+						mysqli_stmt_store_result($stmt);
+						if(mysqli_stmt_num_rows($stmt)==0)
+						{
+							echo "<h3>No flights are available !</h3>";
+						}
+						else
+						{
+							echo "<form action=\"book_tickets2.php\" method=\"post\">";
+							echo "<table cellpadding=\"10\"";
+							echo "<tr><th>Flight No.</th>
+							<th>Origin</th>
+							<th>Destination</th>
+							<th>Departure Date</th>
+							<th>Departure Time</th>
+							<th>Arrival Date</th>
+							<th>Arrival Time</th>
+							<th>Price(Economy)</th>
+							<th>Select</th>
+							</tr>";
+							while(mysqli_stmt_fetch($stmt)) {
+        						echo "<tr>
+        						<td>".$flight_no."</td>
+        						<td>".$from_city."</td>
+								<td>".$to_city."</td>
+								<td>".$departure_date."</td>
+								<td>".$departure_time."</td>
+								<td>".$arrival_date."</td>
+								<td>".$arrival_time."</td>
+								<td>&#x20b9; ".$price_economy."</td>
+								<td><input type=\"radio\" name=\"select_flight\" value=\"".$flight_no."\"></td>
+        						</tr>";
+    						}
+    						echo "</table> <br>";
+    						echo "<input type=\"submit\" value=\"Select Flight\" name=\"Select\">";
+    						echo "</form>";
+    					}
+					}
+					else if($class="business")
+					{
+						$query="SELECT flight_no,from_city,to_city,departure_date,departure_time,arrival_date,arrival_time,price_business FROM Flight_Details where from_city=? and to_city=? and departure_date=? and seats_business>=? ORDER BY  departure_time";
+						$stmt=mysqli_prepare($dbc,$query);
+						mysqli_stmt_bind_param($stmt,"sssi",$origin,$destination,$dep_date,$no_of_pass);
+						mysqli_stmt_execute($stmt);
+						mysqli_stmt_bind_result($stmt,$flight_no,$from_city,$to_city,$departure_date,$departure_time,$arrival_date,$arrival_time,$price_business);
+						mysqli_stmt_store_result($stmt);
+						if(mysqli_stmt_num_rows($stmt)==0)
+						{
+							echo "<h3>No flights are available !</h3>";
+						}
+						else
+						{
+							echo "<form action=\"book_tickets2.php\" method=\"post\">";
+							echo "<table cellpadding=\"10\"";
+							echo "<tr><th>Flight No.</th>
+							<th>Origin</th>
+							<th>Destination</th>
+							<th>Departure Date</th>
+							<th>Departure Time</th>
+							<th>Arrival Date</th>
+							<th>Arrival Time</th>
+							<th>Price(Business)</th>
+							<th>Select</th>
+							</tr>";
+							while(mysqli_stmt_fetch($stmt)) {
+        						echo "<tr>
+        						<td>".$flight_no."</td>
+        						<td>".$from_city."</td>
+								<td>".$to_city."</td>
+								<td>".$departure_date."</td>
+								<td>".$departure_time."</td>
+								<td>".$arrival_date."</td>
+								<td>".$arrival_time."</td>
+								<td>&#x20b9; ".$price_business."</td>
+								<td><input type=\"radio\" name=\"select_flight\" value=".$flight_no."></td>
+        						</tr>";
+    						}
+    						echo "</table> <br>";
+    						echo "<input type=\"submit\" value=\"Select Flight\" name=\"Select\">";
+    						echo "</form>";
+    					}
+					}	
+					mysqli_stmt_close($stmt);
+					mysqli_close($dbc);
+					// else
+					// {
+					// 	echo "Submit Error";
+					// 	echo mysqli_error();
+					// }
+				}
+				else
+				{
+					echo "The following data fields were empty! <br>";
+					foreach($data_missing as $missing)
+					{
+						echo $missing ."<br>";
+					}
+				}
+			}
+			else
+			{
+				echo "Search request not received";
+			}
+		?>
+	</body>
+</html>
